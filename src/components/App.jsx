89ee230bca4885f54ed30503c0e12cd2d2139824
Orest Maryniuk <98083499+ormar90@users.noreply.github.com>
2022-09-25 14:37:11 +0300

    created start files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import { Profile } from './Profile/Profile';
+import user from './Profile/user.json';
+
+const userN = user;
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile userN/>
     </div>
   );
 };
